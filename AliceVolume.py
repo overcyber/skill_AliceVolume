ace729557d755673b5fe5f9f20c9f6ef7e29954f
LazzaAU <larry@fulltimecaravanning.com.au>
2020-10-23 22:42:07 +1000

    tweaked dialogue responces to account for incrementing sound volume.

--- AliceVolume.py
+++ AliceVolume.py
@@ -1,4 +1,3 @@
-
 import re
 from core.base.model.AliceSkill import AliceSkill
 from core.dialog.model.DialogSession import DialogSession
@@ -11,14 +10,15 @@ class AliceVolume(AliceSkill):
 	Description: A simple skill to Control the volume of Alice
 	"""
 
-	_SPOKEN_LEVEL = int
+	_ADJUSTED_VOLUME = 0
+
 
 	@IntentHandler('adjustVolume')
 	@IntentHandler('setSoundLevel')
 	def listenForVolumeRequest(self, session: DialogSession, **_kwargs):
 
 		level = ""
-		AliceVolume._SPOKEN_LEVEL = int(float(f'{session.slotValue("volume")}'))
+
 		card = self.getConfig('audioOut')
 
 		# if no card selected defaultt to card0
@@ -32,12 +32,12 @@ class AliceVolume(AliceSkill):
 
 			if 'lowerVolume' in session.slots:
 				level = f'{session.slotValue("volume")}%-'
+				AliceVolume._ADJUSTED_VOLUME = 1
 			elif 'raiseVolume' in session.slots:
 				level = f'{session.slotValue("volume")}%+'
+				AliceVolume._ADJUSTED_VOLUME = 2
 
-
-		self.adjustVolumePi(level=level,card=card, sessionId=session.sessionId)
-
+		self.adjustVolumePi(level=level, card=card, sessionId=session.sessionId)
 
 
 	def adjustVolumePi(self, level: str, card: str, sessionId):
@@ -47,15 +47,23 @@ class AliceVolume(AliceSkill):
 		# extract the portName
 		portName = re.findall(r"'(.*?)'", portResult.stdout.decode('utf-8'))
 		# run the volume changing system command
+
 		result = self.Commons.runSystemCommand(f'amixer {card} -M -q set {portName[0]} {level}'.split())
 
 		# if no error code
 		if not result.returncode:
+			# get the true volume level for the dialog output
+			amixerResult = self.Commons.runSystemCommand('amixer')
+			spokenLevel = re.findall(r"\[(.*?)]", amixerResult.stdout.decode('utf-8'))
+
+			action = ['set', 'lowered', 'raised']
 
 			self.endDialog(
 				sessionId=sessionId,
-				text=self.randomTalk(text="dialogMessage2", replace=[AliceVolume._SPOKEN_LEVEL])
+				text=self.randomTalk(text="dialogMessage2", replace=[action[AliceVolume._ADJUSTED_VOLUME], spokenLevel[0]])
 			)
+
+			AliceVolume._ADJUSTED_VOLUME = 0
 
 
 		else:
@@ -63,6 +71,5 @@ class AliceVolume(AliceSkill):
 				sessionId=sessionId,
 				text=self.randomTalk(text="dialogMessage1")
 			)
-
+			AliceVolume._ADJUSTED_VOLUME = 0
 			return self.logError(f'Error with {self.name} configuration. Have you selected the right card ?: {result.stderr}')
-
